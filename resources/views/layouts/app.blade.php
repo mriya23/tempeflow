c20ceac36a9d66a980b390dcf35d8cf6baa9fbbb
mriya23 <riskijayakarta2310@gmail.com>
2025-12-24 04:37:49 +0700

    feat: Update favicon to user provided transparent v3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Tempe Jaya Mandiri') }} - Admin</title>
-        <link rel="icon" href="{{ asset('images/favicon_v2.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/favicon_v3.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
