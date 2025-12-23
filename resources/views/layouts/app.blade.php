29f310b93e0bfd54cdd3644c0e0fdbf94abaeab4
mriya23 <riskijayakarta2310@gmail.com>
2025-12-24 04:27:01 +0700

    feat: Add optimized square TJM favicon and update layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Tempe Jaya Mandiri') }} - Admin</title>
-        <link rel="icon" href="{{ asset('images/vector_tempe.png') }}">
+        <link rel="icon" href="{{ asset('images/favicon_tjm.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
